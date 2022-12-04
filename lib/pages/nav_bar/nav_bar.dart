fcf0d7a19299195f709550ace85b882ef3b589f8
Olges <69785258+Olges@users.noreply.github.com>
2022-12-05 05:00:53 +0600

    qr + map ver 1

--- lib/pages/nav_bar/nav_bar.dart
+++ lib/pages/nav_bar/nav_bar.dart
@@ -1,13 +1,18 @@
-import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
-import '../constants/colors.dart';
-import '../translations/locale_keys.g.dart';
+import 'package:flutter/material.dart';
+// import 'package:login_page_hw/pages/navi_pages/profile/ui/profile_page.dart';
+// import 'package:login_page_hw/pages/navi_pages/qr/ui/qr_page.dart';
+
+ import 'package:form_ui/constants/colors.dart';
+import 'package:form_ui/translations/locale_keys.g.dart';
+
+// import 'navi_pages/animation/ui/animation_page.dart';
+// import 'navi_pages/news/ui/news_page.dart';
+// import 'navi_pages/map/ui/map_page.dart';
 
 import 'package:form_ui/pages/nav_wid/home_page.dart';
 import 'package:form_ui/pages/nav_wid/QR/qr_page.dart';
 import 'package:form_ui/pages/nav_wid/profile/profile_page.dart';
-
-//import 'form_ui/map_page.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -22,6 +27,7 @@ class _WelcomePageState extends State<WelcomePage> {
     HomePage(),
     QRpage(),
     //MapPage(),
+    //AnimationPage(),
     ProfilePage()
   ];
 
@@ -43,7 +49,7 @@ class _WelcomePageState extends State<WelcomePage> {
             _currentIndex = newIndex;
           });
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
             activeIcon: Icon(Icons.home),
@@ -58,6 +64,11 @@ class _WelcomePageState extends State<WelcomePage> {
             icon: Icon(Icons.map_outlined),
             activeIcon: Icon(Icons.map),
             label: 'Map',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.animation_outlined),
+            activeIcon: Icon(Icons.animation),
+            label: 'Animation',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
